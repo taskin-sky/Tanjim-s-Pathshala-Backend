f9921f7ee0d1edbb110aada77aa8ce95d5f8a5e3
Taskin Mohammad Mubassir <taskinmubassir@gmail.com>
2026-05-06 00:05:19 +0600

    backend

--- src/server.js
+++ src/server.js
@@ -60,9 +60,6 @@ app.get('/', (req, res) => {
   res.send("✅ Tanjim's Pathshala API is running...");
 });
 
-// ✅ FOR LOCAL DEVELOPMENT: Conditionally start the server
-if (process.env.NODE_ENVIRONMENT !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
